12daf4fb56beac64d9651a5443a5ef7bdf838a2e
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2022-01-21 17:23:47 +0800

    fix: fix linux support

--- app/linux/flutter/generated_plugin_registrant.cc
+++ app/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <kraken/kraken_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) kraken_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "KrakenPlugin");
+  kraken_plugin_register_with_registrar(kraken_registrar);
 }
--- build_linux.sh
+++ build_linux.sh
@@ -1,24 +1,38 @@
+#!/bin/bash
+
 ROOT=$(pwd)
 
 clean() {
-    rm -rf $ROOT/build/darwin/debug/
-    rm -rf $ROOT/build/darwin/release/
+  rm -rf $ROOT/build/linux
+  rm -rf build
 }
 
 build_release() {
-    cd $ROOT/app
-    flutter clean
-    flutter build linux --release
-    mkdir -p $ROOT/build/darwin/release/app.app
-    mv $ROOT/app/build/macos/Build/Products/Release/app.app $ROOT/build/darwin/release
+  cd $ROOT/app
+  flutter clean
+  flutter build linux --release
+  ARCH=$(arch)
+  if [[ "$ARCH" == "x86_64" ]]; then
+    mkdir -p $ROOT/build/linux/release/
+    mv $ROOT/app/build/linux/x64/release/bundle $ROOT/build/linux/release
+  else
+    echo "Only x86_64 support from now on, maybe someone can add more archs."
+    exit 1
+  fi
 }
 
 build_debug() {
-    cd $ROOT/app
-    flutter clean
-    flutter build linux --debug
-    mkdir -p $ROOT/build/darwin/debug/app.app
-    mv $ROOT/app/build/macos/Build/Products/Debug/app.app $ROOT/build/darwin/debug
+  cd $ROOT/app
+  flutter clean
+  flutter build linux --debug
+  ARCH=$(arch)
+  if [[ "$ARCH" == "x86_64" ]]; then
+    mkdir -p $ROOT/build/linux/debug/
+    mv $ROOT/app/build/linux/x64/debug/bundle $ROOT/build/linux/debug
+  else
+    echo "Only x86_64 support from now on, maybe someone can add more archs."
+    exit 1
+  fi
 }
 
 clean
