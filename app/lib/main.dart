9868bda3dbe55a406bceb3585dacedb4be26b8d4
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-03-28 19:48:41 +0800

    fix: bundle must pass after 0.10.3

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kraken/devtools.dart';
 import 'package:kraken_websocket/kraken_websocket.dart';
@@ -5,16 +7,51 @@ import 'package:kraken_video_player/kraken_video_player.dart';
 import 'package:kraken_webview/kraken_webview.dart';
 import 'package:kraken/kraken.dart';
 
+const String BUNDLE_URL = 'KRAKEN_BUNDLE_URL';
+const String BUNDLE_PATH = 'KRAKEN_BUNDLE_PATH';
+
+String? getBundleURLFromEnv() {
+  return Platform.environment[BUNDLE_URL];
+}
+
+String? getBundlePathFromEnv() {
+  return Platform.environment[BUNDLE_PATH];
+}
+
 class MyApp extends StatelessWidget {
+  MyApp() {
+    _constructBundle();
+  }
+
+  KrakenBundle? _bundle;
+
+  void _constructBundle() {
+    String? bundleUrl = getBundleURLFromEnv();
+    print('bundleUrl is $bundleUrl');
+    if (bundleUrl != null) {
+      _bundle = KrakenBundle.fromUrl(bundleUrl);
+    }
+    String? bundlePath = getBundlePathFromEnv();
+    print('bundlePath is $bundlePath');
+    if (bundlePath != null) {
+      _bundle = KrakenBundle.fromUrl('file://$bundlePath');
+    }
+
+    if (_bundle == null) {
+      print('Can not get bundle!');
+      exit(1);
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Kraken Browser',
-        // theme: ThemeData.dark(),
-        home: Kraken(
-            devToolsService: ChromeDevToolsService(),
-            background: Color(0xFFFFFFFF)));
+      title: 'Kraken Browser',
+      home: Kraken(
+        devToolsService: ChromeDevToolsService(),
+        bundle: _bundle,
+        background: Color(0xFFFFFFFF)));
   }
 }
 
